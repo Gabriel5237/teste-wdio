54447affe3adb729a8f3bd21e8316470453a2984
Gabriel Henrique <gabriel55237@gmail.com>
2026-03-25 18:52:34 -0300

    automações appium

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,21 +1,28 @@
 class LoginPage {
     //Seletores
+
+    // 1 - O usuário acessa o menu de login do aplicativo
     get menuLogin() {
         return $('~Login')
     }
-
+    
+    // 2 O sistema exibe os campos de email e senha
+    // 3 O usuário preenche os dados de acesso (email e senha)
     get campoEmail() {
         return $('~input-email')
     }
-
+    
     get campoSenha() {
         return $('~input-password')
     }
-
+    
+    // 4 O usuário aciona o botão de login
     get botaoLogin() {
         return $('~button-LOGIN')
     }
-
+    
+    // 5 O sistema realiza a validação das credenciais informadas
+    // 6 Em caso de sucesso, o sistema exibe a mensagem: "You are logged in!"
     get mensagem() {
         return $('id=android:id/message')
     }
@@ -27,7 +34,7 @@ class LoginPage {
 
     async preencherLogin(email, senha) {
        await this.campoEmail.clearValue()
-       await this.campoEmail.setValue (email)
+       await this.campoEmail.setValue(email)
        await this.campoSenha.clearValue()
        await this.campoSenha.setValue(senha)
        await this.botaoLogin.click()
@@ -36,7 +43,7 @@ class LoginPage {
     async mensagemAlerta() {
         return await this.mensagem.getText()
     }
-
+    // Fluxo alternativo - mensagens de erro
     async mensagemErro(texto) {
         const elemento = await $(`//android.widget.TextView[@text="${texto}"]`)
         await expect(elemento).toHaveText(texto)
--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -1,28 +1,38 @@
 import loginPage from "../pageobjects/login.page";
 
 describe('Funcionalidade: Login', () => {
-
+   
+   // Executa antes de cada teste
    beforeEach( async() => {
+       // Acessa o menu de login
       await loginPage.abrirMenu()
    });
-
+   
+   // Executa após cada teste
    afterEach( async() => {
+      // Reinicia o app para garantir isolamento dos testes
       await browser.relaunchActiveApp()
    });
 
-    it('Deve fazer login com sucesso', async() => {       
+    it('Deve fazer login com sucesso', async() => {
+       // Preenche email e senha válidos       
        await loginPage.preencherLogin('teste@teste.com', 'senha123456')
-       expect(await loginPage.mensagemAlerta()).toEqual('You are logged in!')
+       // Valida mensagem de sucesso
+       await expect(await loginPage.mensagemAlerta()).toEqual('You are logged in!')
     });
 
     it('Deve falhar ao fazer login com email inválido', async() => {
+       // Preenche email inválido
        await loginPage.preencherLogin('teste@teste', 'senha123456')
+       // Valida mensagem de erro
        await loginPage.mensagemErro('Please enter a valid email address')
        
     });
 
     it('Deve falhar ao fazer login com a senha inválida', async() => {
+      // Preenche senha inválida
       await loginPage.preencherLogin('teste@teste.com', '1234')
+      // Valida mensagem de erro
       await loginPage.mensagemErro('Please enter at least 8 characters')
       
     });
